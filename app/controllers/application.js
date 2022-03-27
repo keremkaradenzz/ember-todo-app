be9efa2e9bcdad40550dd26133d3387b393d39db
kerem <keremkaradenzz@gmail.com>
2022-03-27 14:35:08 +0530

    development update todo

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,14 +2,23 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 export default class ApplicationController extends Controller {
+  index = -1;
   @tracked _myValue = '';
   @tracked todo = ['firstTodo'];
+  @tracked isEdit = false;
+
   @action
   myValue(event) {
     event.preventDefault();
     this._myValue = event.target.value;
   }
-
+  @action
+  handleEdit(index) {
+    this.index = index;
+    this._myValue = this.todo[index];
+    this.isEdit = true;
+  }
+  
   @action 
   handleDelete(index) {
     let filtered = this.todo.filter((item, i) => i !== index);
@@ -20,6 +29,17 @@ export default class ApplicationController extends Controller {
   handleClick() {
     if (this._myValue.length > 0) {
       this.todo = [...this.todo, this._myValue];
+    }
+  }
+
+  @action
+  handleUpdate(){
+    if (this._myValue.length > 0 && this.index >= 0) {
+      let filtered = [...this.todo]
+      filtered[this.index] = this._myValue;
+      this.todo = [...filtered];
+      this.isEdit = false;
+      this._myValue = "";
     }
   }
 }
